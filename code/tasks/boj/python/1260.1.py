2d25cca6917b2c2ac8c66a19063ef7903bc63190
Park, Jonghyeon <belline0124@gmail.com>
2021-03-18 00:33:38 +0900

    2021-03-18-a

--- code/tasks/boj/python/1260.1.py
+++ code/tasks/boj/python/1260.1.py
@@ -1,3 +1,4 @@
+# 리스트로 구현
 from sys import stdin
 n, m, v = map(int, stdin.readline().split())
 dots = dict(enumerate([[] for i in range(n)], start=1))
@@ -5,6 +6,8 @@ for _ in range(m):
     a, b = map(int, stdin.readline().split())
     dots[a] += [b]
     dots[b] += [a]
+for j in dots:
+    dots[j].sort()
 v_dfs = []
 v_bfs = []
 
@@ -18,15 +21,17 @@ def dfs(n):
 def bfs(n):
     global v_bfs
     queue = [n]
+    i = 0
     while queue:
         dot = queue.pop(0)
         if dot not in v_bfs:
-            v_bfs += [n]
-            queue += list(set(dots[n]) - set(v_bfs))
+            v_bfs += [dot]
+            queue += dots[dot]
 
-dfs(1)
-bfs(1)
+dfs(v)
+bfs(v)
 for _ in v_dfs:
     print(_, end = ' ')
+print()
 for _ in v_bfs:
     print(_, end = ' ')
