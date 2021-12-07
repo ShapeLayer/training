2b2c2d6c40fa6eda35bd580d6a7a2cf1d2233503
Park, Jonghyeon <belline0124@gmail.com>
2021-12-07 16:41:05 +0900

    2021-12-07-b

--- tasks/jnu/python/clock/app/ui.py
+++ tasks/jnu/python/clock/app/ui.py
@@ -3,16 +3,6 @@ from datetime import date, datetime
 from calendar import monthrange
 from math import pi
 
-def load_clock_str(type_: str) -> str:
-    if type_ not in ['primary', 'secondary', 'primary_sub']:
-        raise ValueError('"type_" must be "primary" or "secondary"')
-    elif type_ == 'primary':
-        return datetime.now().strftime('%H시 %M분')
-    elif type_ == 'primary_sub':
-        return datetime.now().strftime(':%S')
-    elif type_ == 'secondary':
-        return datetime.now().strftime('%Y년 %m월 %d일')
-
 def clock_value_handler():
     if not document.nowtime_lock:
         document.nowtime = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S.%f')
@@ -75,38 +65,3 @@ def write_calendar(dt: datetime) -> str:
         tr_body = tr_body.format(tds)
         table_body += tr_body
     return table_body
-
-def draw_base(ctx, rad):
-    ctx.translate(rad, rad)
-    rad *= .9
-    ctx.arc(0, 0, rad, 0, 2 * pi)
-    ctx.fillStyle = '#fff'
-    ctx.fill()
-
-def draw_numbers (ctx, rad):
-    ctx.font = rad * .15 + 'px SpoqaHanSansNeo'
-    console.log(2)
-    ctx.textBaseline = 'middle'
-    console.log(1)
-    ctx.textAlign = 'center'
-    for i in range(3, 13, 3):
-        console.log(True)
-        angle = i * pi / 6
-        ctx.fillStyle = '#000'
-        ctx.rotate(angle)
-        ctx.translate(0, -rad * .75)
-        ctx.rotate(-angle)
-        ctx.fillText(str(i), 0, 0)
-        ctx.rotate(angle)
-        ctx.translate(0, rad * .75)
-        ctx.rotate(-angle)
-
-def draw_hand(ctx, pos, length, width):
-    ctx.beginPath()
-    ctx.lineWidth = width
-    ctx.lineCap = 'round'
-    ctx.moveTo(0, 0)
-    ctx.rotate(pos)
-    ctx.lineTo(0, -length)
-    ctx.stroke()
-    ctx.rotate(-pos)
