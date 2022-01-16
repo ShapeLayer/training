a5e4e906e84acad68932f07254ca97e28584bb8e
Park, Jonghyeon <belline0124@gmail.com>
2022-01-16 15:48:05 +0900

    2021-01-16-b

--- tasks/oj/boj/rust/2178.rs
+++ tasks/oj/boj/rust/2178.rs
@@ -22,29 +22,31 @@ fn main() {
     }
     map.append(&mut vec![mapline]);
   }
+
+  // BFS
   visit[0][0] = true;
   let mut queue: VecDeque<(i32, i32)> = VecDeque::new();
   queue.push_back((0, 0));
   let dx = vec![-1, 1, 0, 0];
   let dy = vec![0, 0, 1, -1];
-  let mut cnt = 0;
   let mut trace: Vec<Vec<i32>> = vec![vec![0; m as usize];n as usize];
   while queue.len() != 0 {
     let (x, y) = queue.pop_front().unwrap();
-    cnt += 1;
-    trace[y as usize][x as usize] = cnt;
     for i in 0..4 {
       let nx = x + dx[i];
       let ny = y + dy[i];
       if nx < 0 || nx >= m || ny < 0 || ny >= n { continue; }
       if !map[ny as usize][nx as usize] { continue; }
       if visit[ny as usize][nx as usize] { continue; }
+      trace[ny as usize][nx as usize] = trace[y as usize][x as usize] + 1;
       visit[ny as usize][nx as usize] = true;
       queue.push_back((nx, ny));
     }
   }
-  println!("{}", cnt);
+
+  /* // Printing step map.
   for i in 0..n {
     println!("{:?}", trace[i as usize]);
-  }
+  }*/
+  println!("{}", trace[(n as usize) - 1][(m as usize) - 1] + 1)
 }
