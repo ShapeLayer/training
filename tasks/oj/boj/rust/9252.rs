27463b2825131293e979c2d2f8741e6f5491581e
Park, Jonghyeon <belline0124@gmail.com>
2022-01-30 14:04:48 +0900

    2021-01-30-b

--- tasks/oj/boj/rust/9252.rs
+++ tasks/oj/boj/rust/9252.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{io, cmp};
 
 fn main() {
   let mut puts = String::new();
@@ -11,32 +11,38 @@ fn main() {
   let str_b: Vec<String> = puts.trim().chars()
     .map(|i| i.to_string())
     .collect();
-  let n = str_a.len();
-  let mut map: Vec<Vec<String>> = vec![vec![String::new(); n as usize]; n as usize];
-  let mut dots: Vec<(i32, i32)> = Vec::new();
-  for i in 0..n {
-    for j in 0..n {
-      if str_a[i as usize] == str_b[j as usize] {
-        map[i as usize][j as usize] = str_a[i as usize].clone();
-        dots.append(&mut vec![(i as i32, j as i32)]);
+  let na = str_a.len();
+  let nb = str_b.len();
+  let mut map: Vec<Vec<i32>> = vec![vec![0; (nb+1) as usize]; (na+1) as usize];
+  for i in 1..na+1 {
+    for j in 1..nb+1 {
+      // String indexing: i-1
+      // `map` length is n+1 * n+1
+      if str_a[(i-1) as usize] == str_b[(j-1) as usize] {
+        map[i as usize][j as usize] = map[(i-1) as usize][(j-1) as usize] + 1;
+      } else {
+        map[i as usize][j as usize] = cmp::max(map[(i-1) as usize][j as usize], map[i as usize][(j-1) as usize]);
       }
     }
   }
-  for dot in &dots {
-    let (mut maximum, mut idx) = (String::new(), (0, 0));
-    for target in &dots {
-      if target.0 <= dot.0 && target.1 <= dot.1 {
-        if map[target.0 as usize][target.1 as usize].len() > maximum.len() {
-          maximum = map[target.0 as usize][target.1 as usize].clone();
-          idx = *target;
-        }
-      }
+
+  let mut lcs = String::new();
+  let (mut i, mut j) = (na, nb);
+  while i != 0 && j != 0 {
+    let now = map[i as usize][j as usize];
+    if now != map[(i-1) as usize][j as usize] && now != map[i as usize][(j-1) as usize] {
+      lcs.push_str(&str_a[(i-1) as usize].clone());
     }
-    map[dot.0 as usize][dot.1 as usize] = maximum.clone() + &map[dot.0 as usize][dot.1 as usize].clone();
-    println!("dot: {:?}, mapcurr: {}", dot, map[dot.0 as usize][dot.1 as usize])
+    /*if now == map[(i-1) as usize][j as usize] { i -= 1; }
+    else if now == map[i as usize][(j-1) as usize] { j -= 1; }
+    else { i-=1; j-=1; }*/
+    
+    if now != map[(i-1) as usize][j as usize] { j -= 1; }
+    if now != map[i as usize][(j-1) as usize] { i -= 1; }
+    
+    if now == 0 { break; }
   }
-  for arr in map {
-    println!("{:?}", arr);
-  }
-  println!("{:?}", dots);
+  lcs = lcs.chars().rev().collect::<String>();
+  println!("{}", lcs.len());
+  println!("{}", lcs);
 }
