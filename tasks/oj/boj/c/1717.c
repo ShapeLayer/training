e9740923d7e476ab4f68a960b273aa4c0c3d16dd
Park, Jonghyeon <belline0124@gmail.com>
2022-03-08 13:49:27 +0900

    2022-03-08-a

---
+++ tasks/oj/boj/c/1717.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int parents[1000001];
+
+int find(int n) {
+  if (parents[n] == n) return n;
+  return find(parents[n]);
+}
+
+void merge(int a, int b) {
+  int pa = find(a), pb = find(b);
+  if (pa > pb) parents[pa] = pb;
+  else if (pa < pb) parents[pb] = pa;
+}
+
+int main() {
+  int n, m;
+  for (int i = 1; i < 1000001; i++) parents[i] = i;
+  scanf("%d %d", &n, &m);
+  for (int i = 0; i < m; i++) {
+    int o, a, b;
+    scanf("%d %d %d", &o, &a, &b);
+    if (o == 0) merge(a, b);
+    else {
+      if (find(a) == find(b)) printf("YES\n");
+      else printf("NO\n");
+    }
+  }
+}
