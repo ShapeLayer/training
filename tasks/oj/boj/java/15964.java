9aa5d0ca10801b6876b111ba0640e02d239ea86b
Park, Jonghyeon <belline0124@gmail.com>
2022-04-02 14:06:59 +0900

    2022-04-02-b

---
+++ tasks/oj/boj/java/15964.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    long a = sc.nextLong(), b = sc.nextLong();
+    System.out.println((a+b)*(a-b));
+  }
+}
