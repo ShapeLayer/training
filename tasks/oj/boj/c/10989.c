3ea3098dfaf927b375a04ffd14d4c6b42b96a93b
Park, Jonghyeon <belline0124@gmail.com>
2022-04-11 00:10:52 +0900

    2022-04-11-a

---
+++ tasks/oj/boj/c/10989.c
@@ -0,0 +1,16 @@
+// Counting Sort
+
+#include <stdio.h>
+
+int main() {
+  int cnt[10001] = { 0 };
+  int n, gets;
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &gets);
+    cnt[gets]++;
+  }
+  for (int i = 0; i < 10001; i++)
+    for (int j = 0; j < cnt[i]; j++)
+      printf("%d\n", i);
+}
