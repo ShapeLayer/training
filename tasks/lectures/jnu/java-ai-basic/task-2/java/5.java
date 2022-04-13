7d5babe9fcf43eb9b69c972945e80759675f0d2e
Park, Jonghyeon <belline0124@gmail.com>
2022-04-13 20:23:58 +0900

    Update java lecture tex report

--- tasks/lectures/jnu/java-ai-basic/task-2/java/5.java
+++ tasks/lectures/jnu/java-ai-basic/task-2/java/5.java
@@ -12,7 +12,7 @@ public class Main {
     System.out.print("금액을 입력하시오 >> ");
     remains = sc.nextInt();
     for (int u: unit) { // foreach문 사용, u는 unit의 요소(단위)
-      int n = remains / u; // 잔액을 단위로 나눈 몫 구하기
+      int n = remains / u; // 잔액을 단위로 나눈 몫 구하기 (=지폐 혹은 화폐의 개수)
       if (n == 0) continue; // 몫이 0이면 다음 단위 처리
       System.out.println(String.format("%d원: %d개", u, n));
       remains -= u*n;
