34528f365a3237909d9992c5385606bd91745aef
Park, Jonghyeon <belline0124@gmail.com>
2022-05-11 15:10:13 +0900

    Done linux hw 7

--- tasks/lectures/jnu/linux-ce-park-2022/task7/main.c
+++ tasks/lectures/jnu/linux-ce-park-2022/task7/main.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main() {
   int pid = fork();
-  /*
-  for (int i = 0; i < 4; i++) {
-    int pid = fork();
+  for (int i = 0; i < 3; i++) {
     if (pid == 0) {
-      printf("pid: %d\n", getpid());
-      exit(0);
-    }
-  }*/
-  if (pid > 0) {
-    printf("pid: %d\n", getpid());
-    for (int i = 0; i < 4; i++) {
       pid = fork();
-      printf("pid: %d\n", getpid());
     }
   }
-  return 0;
+  int waiting_child_pid;
+  wait(&waiting_child_pid);
+  printf("pid: %d\n", getpid());
 }
