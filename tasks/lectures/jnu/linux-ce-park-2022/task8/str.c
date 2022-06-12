73e733c382ae0321a12b4cc8299157344bd4ea9e
Park, Jonghyeon <belline0124@gmail.com>
2022-06-13 01:04:36 +0900

    Update task 8

--- tasks/lectures/jnu/linux-ce-park-2022/task8/str.c
+++ tasks/lectures/jnu/linux-ce-park-2022/task8/str.c
@@ -1,7 +1,9 @@
 #include <assert.h> /* to use assert() */
 #include <stdio.h>
 #include <string.h>
-#include "str.h" #include <ctype.h>
+#include <stdlib.h>
+#include "str.h"
+#include <ctype.h>
 
 /* Your task is:
 Rewrite the body of "Part 1" functions - remove the current body that simply calls the corresponding C
@@ -21,7 +23,14 @@ char *StrToLower(char *str)
 {
   char *str_clone;
   str_clone = (char *)malloc(1024);
-  int count=0;
+  int count = 0;
   /* TODO: fill this function */
   /* Part 1 */
+  for (int i = 0; i < MAX_STR_LEN; i++) {
+    if (str[i] >= 65 && str[i] <= 90) {
+      str_clone[i] = str[i] + 32;
+    } else {
+      str_clone[i] = str[i];
+    }
+  }
 }
--- tasks/lectures/jnu/linux-ce-park-2022/task8/str.h
+++ tasks/lectures/jnu/linux-ce-park-2022/task8/str.h
@@ -1,6 +1,6 @@
 #ifndef _STR_H_
 #define _STR_H_
-#include <limits.h> /* for MAX_STR_LEN */
+#include "limits.h" /* for MAX_STR_LEN */
 #include <unistd.h> /* for typedef of size_t */
 size_t StrGetLength(const char* pcSrc);
 char *StrToLower(char *str);
